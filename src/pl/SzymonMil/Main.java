f56ceb55ad0daf149b71517ec150609ed2ea1159
Szymon <seltinxx@gmail.com>
2022-01-30 19:53:49 +0100

    commit

---
+++ src/pl/SzymonMil/Main.java
@@ -0,0 +1,33 @@
+package pl.SzymonMil;
+
+import java.util.*;
+import java.io.FileNotFoundException;
+import java.util.*;
+import java.io.File;
+
+public class Main
+{
+    public static void main(String[] args) throws FileNotFoundException
+    {
+        // Klasa TreeSet implementuje interfejs Set
+        Set<String> words = new TreeSet<>();
+        long totalTime = 0;
+
+        Scanner in = new Scanner(new File("C:\\Users\\Szymon\\Desktop\\WordsInHashSet\\alice30.txt"));
+        while (in.hasNext()) {
+            String word = in.next();
+            long callTime = System.currentTimeMillis();
+            words.add(word);
+            callTime = System.currentTimeMillis() - callTime;
+            totalTime += callTime;
+        }
+
+        Iterator<String> iter = words.iterator();
+        for (int j = 1; j <= 20; ++j) {
+            System.out.println(iter.next());
+        }
+
+        System.out.println(". . .");
+        System.out.println(words.size() + " unikalnych słów. " + totalTime + " milisekund.");
+    }
+}
